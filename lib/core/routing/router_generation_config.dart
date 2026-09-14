c06480243dd6a225ce00116d74f807abe5cfda57
Ahmed Moustafa <am6888122@gmail.com>
2026-09-14 22:42:11 +0300

    Merge remote-tracking branch 'origin/main' into feature/tasks

--- lib/core/routing/router_generation_config.dart
+++ lib/core/routing/router_generation_config.dart
@@ -1,20 +1,12 @@
 import 'package:go_router/go_router.dart';
 import 'package:workwise/core/routing/app_routes.dart';
 import 'package:workwise/core/routing/page_transition.dart';
-import 'package:workwise/features/auth/presentation/screens/sign_in_screen.dart';
 import 'package:workwise/features/tasks/presentation/screens/tasks_screen.dart';
 
 class RouterGenerationConfig {
   static GoRouter goRouter = GoRouter(
     initialLocation: AppRoutes.tasksScreen,
     routes: [
-      GoRoute(
-        name: AppRoutes.signInScreen,
-        path: AppRoutes.signInScreen,
-        pageBuilder: (context, state) =>
-            slideTransitionPage(state: state, child: const SignInScreen()),
-      ),
-
       GoRoute(
         name: AppRoutes.tasksScreen,
         path: AppRoutes.tasksScreen,
