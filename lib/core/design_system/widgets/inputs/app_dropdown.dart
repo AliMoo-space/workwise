21d5493c97e6a9d37aaf007037479e99797ae532
ali <ali>
2026-09-14 12:41:48 +0300

    Add full home and attendance screens

--- lib/core/design_system/widgets/inputs/app_dropdown.dart
+++ lib/core/design_system/widgets/inputs/app_dropdown.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
 import 'package:workwise/core/design_system/colors/app_colors.dart';
 import 'package:workwise/core/design_system/spacing/app_radius.dart';
 import 'package:workwise/core/design_system/spacing/app_spacing.dart';
@@ -77,7 +78,7 @@ class AppDropdown<T> extends StatelessWidget {
               color: AppColors.textPrimary,
             ),
           ),
-          const SizedBox(height: AppSpacing.space8),
+          const Gap(AppSpacing.space8),
         ],
 
         DropdownButtonFormField<T>(
