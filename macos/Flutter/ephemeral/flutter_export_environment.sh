368e040c1f59eabbe219b7d1c9989ea5b44f05f4
Ahmed Moustafa <am6888122@gmail.com>
2026-09-21 16:00:14 +0300

    ef

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=C:\src\flutter"
-export "FLUTTER_APPLICATION_PATH=C:\flutter_projects\workwise"
-export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=C:\flutter_projects\workwise\macos\Flutter\ephemeral\Packages\.packages\FlutterFramework"
+export "FLUTTER_ROOT=C:\medo\flutter"
+export "FLUTTER_APPLICATION_PATH=C:\Users\Ahmed\StudioProjects\workwise"
+export "FLUTTER_FRAMEWORK_SWIFT_PACKAGE_PATH=C:\Users\Ahmed\StudioProjects\workwise\macos\Flutter\ephemeral\Packages\.packages\FlutterFramework"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
